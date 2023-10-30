b055cd954964264b0ae678fbf47f257feb0e2c63
Nicolas <n0hall@enib.fr>
2023-10-29 23:01:49 -0400

    Android 10 base

---
+++ app/src/main/java/com/example/timetravel/ui/home/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.example.timetravel.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.preference.PreferenceManager
+import com.example.timetravel.R
+import com.example.timetravel.databinding.FragmentHomeBinding
+import org.osmdroid.config.Configuration
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+      override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
