b7b641fd32f7fbe0f26ce8d02d3bde92c268466c
Nico Nico <143906787+Nic0Nico@users.noreply.github.com>
2023-11-18 15:12:59 -0500

    localisation utilisateur

--- app/src/main/java/com/example/timetravel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/timetravel/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.timetravel.ui.home
 
 import android.annotation.SuppressLint
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,57 +10,57 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.preference.PreferenceManager
+import com.example.timetravel.LocationManager
 import com.example.timetravel.R
 import com.example.timetravel.databinding.FragmentHomeBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
-import android.os.Bundle
-import com.example.timetravel.LocationManager
-
-
+import org.osmdroid.views.overlay.Marker
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var map: MapView
     private lateinit var locationManager: LocationManager
-      //@SuppressLint("SuspiciousIndentation")
-      override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+    private lateinit var marker: Marker
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-          // Setting up osmdroid configuration
-          Configuration.getInstance()
-              .load(context, requireActivity().getSharedPreferences("OpenStreetMap", 0))
+        // Setting up osmdroid configuration
+        Configuration.getInstance()
+            .load(context, requireActivity().getSharedPreferences("OpenStreetMap", 0))
 
-          // Initializing map view
-          map = root.findViewById(R.id.map)
-          map.setTileSource(TileSourceFactory.MAPNIK)
-          map.setMultiTouchControls(true)
+        // Initializing map view
+        map = root.findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
+        map.controller.setZoom(18.0)
 
+        locationManager = LocationManager(requireActivity())
+        marker = Marker(map)
+        // Appeler la méthode pour demander les mises à jour de localisation
+        locationManager.requestLocationUpdates { latitude, longitude ->
+            //println("Latitude: $latitude, Longitude: $longitude")
 
-          locationManager = LocationManager(requireActivity())
-
-          // Appeler la méthode pour demander les mises à jour de localisation
-          locationManager.requestLocationUpdates { latitude, longitude ->
-              //println("Latitude: $latitude, Longitude: $longitude")
-
-          val startPoint = GeoPoint(latitude, longitude) // Default location is Paris
-          map.controller.setCenter(startPoint)
-          map.controller.setZoom(12.0)
-          }
+            val startPoint = GeoPoint(latitude, longitude) // Default location is Paris
+            map.controller.setCenter(startPoint)
+            marker.position = GeoPoint(latitude, longitude)
+            marker.title = "Ma position"
+            map.overlays.add(marker)
+        }
         return root
     }
 
