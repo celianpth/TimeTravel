66be033ec383b1e1cc222c70e4cb0e4f1ca1d27b
Nico Nico <143906787+Nic0Nico@users.noreply.github.com>
2023-11-19 01:38:41 -0500

    laison firebase marker pour les geopoint

--- app/src/main/java/com/example/timetravel/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/timetravel/ui/dashboard/DashboardFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.timetravel.databinding.FragmentDashboardBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DashboardFragment : Fragment() {
 
@@ -32,6 +34,32 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        val db = FirebaseFirestore.getInstance()
+
+        // Créez une nouvelle carte de données à stocker
+        val data = hashMapOf(
+            "NewData" to "valeur"
+        )
+
+        // Ajoutez un nouveau document à la collection "marker"
+        db.collection("t")
+            .add(data)
+            .addOnSuccessListener { documentReference ->
+                println("DocumentSnapshot ajouté avec l'ID: ${documentReference.id}")
+                Toast.makeText(
+                    requireContext(),
+                    "goog",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .addOnFailureListener { e ->
+                println("Erreur lors de l'ajout du document: $e")
+                Toast.makeText(
+                    requireContext(),
+                    "pas goog",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         return root
     }
 
--- app/src/main/java/com/example/timetravel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/timetravel/ui/home/HomeFragment.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
-import androidx.preference.PreferenceManager
 import com.example.timetravel.LocationManager
 import com.example.timetravel.R
 import com.example.timetravel.databinding.FragmentHomeBinding
@@ -18,6 +18,11 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
@@ -28,6 +33,29 @@ class HomeFragment : Fragment() {
     private lateinit var map: MapView
     private lateinit var locationManager: LocationManager
     private lateinit var marker: Marker
+
+    //var myRef = database.getReference("prout")
+
+
+    private fun addMarker(latitude: Double, longitude: Double, title: String) {
+        val geoPoint = GeoPoint(latitude, longitude)
+        val marker = Marker(map).apply {
+            position = geoPoint
+            this.title = title
+        }
+        map.overlays.add(marker)
+        // Save marker information to Firebase Realtime Database
+        /*val markerData = hashMapOf(
+            "latitude" to latitude,
+            "longitude" to longitude,
+            "name" to title
+        )
+        db.collection("marker_point").add(markerData)*/
+
+
+        //myRef.setValue(latitude)
+    }
+
     @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,7 +76,48 @@ class HomeFragment : Fragment() {
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setMultiTouchControls(true)
         map.controller.setZoom(18.0)
+        val latitudeEditText: EditText = root.findViewById(R.id.latitude)
+        val longitudeEditText: EditText = root.findViewById(R.id.longitude)
+        val addMarkerButton: Button = root.findViewById(R.id.add_marker)
 
+        val db = FirebaseFirestore.getInstance()
+
+        addMarkerButton.setOnClickListener {
+            val latitude = latitudeEditText.text.toString().toDoubleOrNull()
+            val longitude = longitudeEditText.text.toString().toDoubleOrNull()
+            if (latitude != null && longitude != null) {
+                addMarker(latitude,longitude, "Marqueur utilisateur")
+                Toast.makeText(
+                    requireContext(),
+                    "marker ajouter",
+                    Toast.LENGTH_SHORT
+                ).show()
+                val markerData = hashMapOf(
+                    "Latitude" to latitude,
+                    "Longitude" to longitude,
+                    "Name" to "Marqueur utilisateur"
+                )
+                db.collection("marker").add(markerData).addOnSuccessListener { documentReference ->
+                    println("DocumentSnapshot ajouté avec l'ID: ${documentReference.id}")
+                    Toast.makeText(
+                        requireContext(),
+                        "goog",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                    .addOnFailureListener { e ->
+                        println("Erreur lors de l'ajout du document: $e")
+                        Toast.makeText(
+                            requireContext(),
+                            "pas good",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+            } else {
+                // Afficher un message d'erreur à l'utilisateur
+            }
+        }
         locationManager = LocationManager(requireActivity())
         marker = Marker(map)
         // Appeler la méthode pour demander les mises à jour de localisation
@@ -57,12 +126,11 @@ class HomeFragment : Fragment() {
 
             val startPoint = GeoPoint(latitude, longitude) // Default location is Paris
             map.controller.setCenter(startPoint)
-            marker.position = GeoPoint(latitude, longitude)
-            marker.title = "Ma position"
-            map.overlays.add(marker)
+            addMarker(latitude, longitude, "Ma position")
         }
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
