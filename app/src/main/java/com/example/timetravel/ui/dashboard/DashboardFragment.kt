633af382552eedab9f20f31ce5c1aae33bc5a247
Nico Nico <143906787+Nic0Nico@users.noreply.github.com>
2023-11-19 19:55:22 -0500

    ajout dashboard monument musique plus redimensionnement des fragments

--- app/src/main/java/com/example/timetravel/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/timetravel/ui/dashboard/DashboardFragment.kt
@@ -1,27 +1,27 @@
 package com.example.timetravel.ui.dashboard
 
-import android.R
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.timetravel.R
 import com.example.timetravel.databinding.FragmentDashboardBinding
 
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
+    
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    //var mediaPlayer = MediaPlayer.create(this, R.raw.sample)// R.raw.sample est le nom de votre fichier audio
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -33,8 +33,9 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
         (activity as AppCompatActivity).supportActionBar?.hide()
-        /*val btnPlay: Button = root.findViewById(com.example.timetravel.R.id.btnPlaymonument)
-        btnPlay.setOnClickListener { playSound() }*/
+        mediaPlayer = MediaPlayer.create(requireContext(), R.raw.sample)
+        val btnPlay: Button = root.findViewById(com.example.timetravel.R.id.btnPlaymonument)
+        btnPlay.setOnClickListener { playSound() }
 
         dashboardViewModel.text.observe(viewLifecycleOwner) {
 
@@ -42,20 +43,18 @@ class DashboardFragment : Fragment() {
 
         return root
     }
-    /*fun playSound() {
+    fun playSound() {
         if (mediaPlayer != null) {
-            if (!mediaPlayer.isPlaying()) {
-                mediaPlayer.start();
+            if (mediaPlayer?.isPlaying == false) {
+                mediaPlayer?.start()
             }
         }
-    }*/
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        /*if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }*/
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
